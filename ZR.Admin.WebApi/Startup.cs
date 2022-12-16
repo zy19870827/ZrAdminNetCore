5f6f746b8e026111fd4fd513e8a94e5e0737d736
不做码农 <599854767@qq.com>
2022-12-16 15:52:35 +0800

    控制台增加logo

--- ZR.Admin.WebApi/Startup.cs
+++ ZR.Admin.WebApi/Startup.cs
@@ -89,6 +89,9 @@ namespace ZR.Admin.WebApi
             });
 
             services.AddSwaggerConfig();
+
+            // 显示logo
+            services.AddLogo();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
